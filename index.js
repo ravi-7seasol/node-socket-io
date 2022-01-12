d03315403087b34e505f67bd33de22447a7765ef
ravi <ravi.7seasol@gmail.com>
2022-01-12 18:13:13 +0530

    chnages

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "https://react-chat-demo-1.netlify.app/",
+        // origin: "https://react-chat-demo-1.netlify.app/",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 })
