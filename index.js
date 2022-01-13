974e0fe2f9b1cf4180bd692b25e777a63982d5c7
ravi <ravi.7seasol@gmail.com>
2022-01-13 17:06:28 +0530

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 const io = new Server(server, {
     // allowEIO3: true, // false by default
     path: '/socket.io',
-    transports: ['websocket'],
+    // transports: ['websocket'],
     secure: true,
     cors: {
         origin: "https://react-socket-io.vercel.app/",
